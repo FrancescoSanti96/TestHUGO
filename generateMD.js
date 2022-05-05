a7966176a73e079253340bd8b0fd508c5c701c04
Francesco <francescosanti123@gmail.com>
2022-05-05 15:31:25 +0200

    fix script

--- generateMD.js
+++ generateMD.js
@@ -2,14 +2,14 @@
 const fs = require('fs');
 
 
-const rawdata = fs.readFileSync('list_skus.json');
+const rawdata = fs.readFileSync('date.json');
 const object = JSON.parse(rawdata)
 
 // Lunghezza
 // const length = Object.keys(object.data).length
 
 //Ciclo l'oggetto per recuperare i nomi delli sku per generare i gile
-for (const variable of object.data){
+for (const variable of object){
     id = (JSON.stringify(variable.id)).toLowerCase().slice(1,-1)
     fs.writeFile(`./project/content/products/${id}.md`,'' ,function (err) {
         if (err) throw err;
